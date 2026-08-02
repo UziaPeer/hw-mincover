dffe47cef5c1cc5b59cc0700bb3d73fd7b5a300e
Uzia Peer <uzia.Peer@msmail.ariel.ac.il>
2026-08-02 15:08:38 +0300

    Update mincover.py

--- mincover.py
+++ mincover.py
@@ -2,10 +2,14 @@
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxpy"], stdout=subprocess.DEVNULL)
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "networkx>=3.4"], stdout=subprocess.DEVNULL)
 
-import networkx as nx, cvxpy, numpy as np
+import numpy as np
 np.float_ = np.float64
 
-def mincover(graph: nx.Graph)->set:
+import networkx as nx
+import cvxpy
+
+
+def mincover(graph: nx.Graph) -> set:
     """
     Return a minimum-cardinality vertex cover in the given graph.
     
@@ -18,15 +22,15 @@ def mincover(graph: nx.Graph)->set:
     >>> len(mincover(nx.Graph([])))
     0
     """
-    # Your code here
- if graph.number_of_edges() == 0:
+    if graph.number_of_edges() == 0:
         return set()
 
     vertices = list(graph.nodes())
     place = {v: i for i, v in enumerate(vertices)}
-    x = cvxpy.Variable(len(vertices), boolean=True)
 
+    x = cvxpy.Variable(len(vertices), boolean=True)
     constraints = [x[place[u]] + x[place[v]] >= 1 for u, v in graph.edges()]
+
     problem = cvxpy.Problem(cvxpy.Minimize(cvxpy.sum(x)), constraints)
     problem.solve(solver=cvxpy.SCIPY)
 
@@ -41,4 +45,3 @@ if __name__ == '__main__':
     # edges=eval(input())
     # graph = nx.Graph(edges)
     # print(len(mincover(graph)))
-
